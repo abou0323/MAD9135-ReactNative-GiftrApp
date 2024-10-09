2ee7d037af824ded7db9dbde1ce5a75ab823ea2b
Sandra Abou-Eid <abou0323@algonquinlive.com>
2024-10-09 00:57:23 -0400

    starter code

--- App.js
+++ App.js
@@ -1,11 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+// import PeopleScreen from './screens/PeopleScreen';
+// import AddPersonScreen from './screens/AddPersonScreen';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       {/* <PeopleScreen/> */}
+//       <AddPersonScreen/>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+
+
+
+// // import PeopleScreen from "./screens/PeopleScreen";
+// // // import RecipePage from "./pages/RecipePage";
+// // // import IngredientsPage from "./pages/IngredientsPage";
+// // import { createNativeStackNavigator } from "@react-navigation/native-stack";
+// // import { NavigationContainer } from "@react-navigation/native";
+
+// // const Stack = createNativeStackNavigator();
+
+// // export default function App() {
+// //   return (
+// //     <NavigationContainer>
+// //       <Stack.Navigator initialRouteName="PeopleScreen">
+// //         <Stack.Screen name="PeopleScreen" component={PeopleScreen} />
+// //         {/* <Stack.Screen name="Recipe" component={RecipePage} />
+// //         <Stack.Screen name="Ingredients" component={IngredientsPage} /> */}
+// //       </Stack.Navigator>
+// //     </NavigationContainer>
+// //   );
+// // }
+
+
+import { StyleSheet, Text, View } from "react-native";
+import AppNavigator from "./AppNavigator";
+import { PeopleProvider } from "./PeopleContext";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <PeopleProvider>
+        <AppNavigator />
+      </PeopleProvider>
     </View>
   );
 }
@@ -13,8 +62,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
